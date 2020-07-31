c01dfed92c07d7ee4dee32a8ebdcd7bfe6b55303
Ji Simmons <ji.simmons@yahoo.com>
2020-07-30 19:59:34 -0600

    here ya goo

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -28,6 +28,403 @@ export default class Calendar extends Component {
       openEvent: false,
       eventList: "",
       editForm: "",
+      clickedEvent: {},
+      _id: "",
+      event: ""
+    };
+    this.handleClose = this.handleClose.bind(this);
+  }
+
+  componentDidMount(){
+        this.getEvents();
+  }
+
+  categoryUpdate=(category) => {
+      this.setState({category})
+      console.log('categoryUpdate', category);
+  }
+
+  // getCachedEvents(){
+  //   const cachedEvents = localStorage.getItem("cachedEvents");
+  //   console.log("Cached Events", JSON.parse(cachedEvents));
+  //   if(cachedEvents){
+  //       this.setState({events: JSON.parse(cachedEvents)})
+  //   }
+  //   return;
+  // }
+
+  getEvents = () => {
+    //axios
+    fetch('http://localhost:5556/api/events')
+    //.get("http://localhost:5556/api/events")
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        console.log("data from api: ", data);
+        this.setState({
+          events: data   
+        })})
+      .catch(error => console.log(error))   
+  }
+
+  //closes modals
+  handleClose = () => {
+    this.setState({ openEvent: false, openSlot: false })
+  }
+
+  //  Allows user to click on calendar slot and handles if appointment exists
+  handleSlotSelected = (slotInfo) => {
+    console.log("Real slotInfo", slotInfo);
+    this.setState({
+      category: "",
+      title: "",
+      desc: "",
+      start: slotInfo.start,
+      end: slotInfo.end,
+      openSlot: true,
+      _id: ""
+    });
+  }
+
+  handleEventSelected = (event) => {
+    console.log("event", event);
+    this.setState({
+      openEvent: true,
+      clickedEvent: event,
+      start: event.start,
+      end: event.end,
+      title: event.title,
+      desc: event.desc,
+      category: event.category,
+      _id: event._id
+    });
+  }
+
+  setCategory = (e) => {
+    this.setState({ category: e });
+  }
+
+  setTitle(e) {
+    this.setState({ title: e });
+  }
+
+  setDescription(e) {
+    this.setState({ desc: e });
+  }
+
+  handleStartTime = (_event, date) => {
+    console.log("date: ", typeof date);
+    this.setState({ start: date });
+  };
+
+  handleEndTime = (_event, date) => {
+    this.setState({ end: date });
+  };
+
+  // Onclick callback function that pushes new appointment into events array.
+  setNewAppointment(_event) {
+    const { start, category, end, title, desc } = this.state;
+    let appointment = { title, category, start, end, desc };
+    let events = this.state.events.slice();
+    events.push(appointment);
+   
+        // and some other stuff
+		let route = `http://localhost:5556/api/events`;
+    //let route = `https://helio-calendar-api.herokuapp.com/api/events`;
+    let submitData = { ...this.state };
+		delete submitData._id;
+        let options = {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(submitData),
+        };
+        fetch(route, options)
+        .then((res) => { return res.json()})
+        .then((data) =>
+        {
+            console.log('should have added a new event: ', data);
+            // this.context.getEvent;
+            this.getEvents();
+        })
+        .catch((err) =>
+        {
+            console.log('might not have added a new event: ', err);
+        })
+	};
+  
+
+  //  Updates Existing Appointments Title and/or Description
+  updateEvent = (_event) => {
+    const { title, category, desc, start, end, events, clickedEvent, _id } = this.state;
+    const index = events.findIndex(event => event === clickedEvent);
+    const updatedEvent = events.slice();
+    updatedEvent[index].category = category;
+    updatedEvent[index].title = title;
+    updatedEvent[index].desc = desc;
+    updatedEvent[index].start = start;
+    updatedEvent[index].end = end;
+    updatedEvent[index]._id = _id;
+
+    //event.preventDefault();
+		console.log('first state: ', this.state);
+		// and some other stuff
+		let route = 'http://localhost:5556/api/events';
+		//let route = 'https://helio-calendar-api.herokuapp.com/api/events';
+		// we need the _id in state to make stuff work but we don't actually want to submit it
+		let submitData = { ...this.state };
+		delete submitData._id;
+		let fetchOptions = {
+			method: 'POST',
+			headers: { 'Content-Type': 'application/json' },
+			body: JSON.stringify(submitData)
+		};
+		if (this.state._id !== '') {
+			fetchOptions.method = 'PUT';
+			route += `/${this.state._id}`;
+		}
+		// send all of the state data to the update list api endpoint
+		console.log('state: ', this.state);
+		console.log('route: ', route);
+		console.log('fetchOptions: ', fetchOptions);
+		fetch(route, fetchOptions)
+			.then((data) => {
+				return data.json();
+			})
+			.then((result) => {
+				// call get lists again to update my app
+				console.log('result: ', result);
+				this.getEvents();
+			})
+			.catch((err) => {
+				console.log('Error updating/creating event: ', err);
+			});
+  }
+
+  //  filters out specific event that is to be deleted and set that variable to state
+  deleteEvent = (_e, _event) => {
+    //e.preventDefault();
+    // let updatedEvents = this.state.events.filter(
+    //   event => event["start"] !== this.state.start
+    // );
+    console.log('deleting...');
+		
+		let id = this.state.clickedEvent._id
+		console.log('id: ', id);
+		let fetchOptions = {
+			method: 'DELETE'
+		};
+		fetch(`http://localhost:5556/api/events/${id}`, fetchOptions)
+		//fetch(`https://helio-calendar-api.herokuapp.com/api/lists/${id}`, fetchOptions)
+			.then((response) => {
+				return response.json();
+			})
+			.then((data) => {
+				console.log('response from api: ', data);
+        this.getEvents();
+        this.handleClose();
+			})
+			.catch();
+  };
+  
+
+  render() {
+    
+    console.log("render()");
+    const eventActions = [
+      <FlatButton
+        label="Cancel"
+        primary={false}
+        keyboardFocused={true}
+        onClick={this.handleClose}
+      />,
+      <FlatButton
+        label="Delete"
+        secondary={true}
+        keyboardFocused={true}
+        onClick={(e) => {
+        //  this.deleteEvent(), this.handleClose();
+        this.deleteEvent(e)
+        }}
+      />,
+      <FlatButton
+        label="Confirm Edit"
+        primary={true}
+        keyboardFocused={true}
+        onClick={() => {
+          this.updateEvent()
+          this.handleClose();
+        }}
+      />
+    ];
+    const appointmentActions = [
+      <FlatButton label="Cancel" secondary={true} onClick={this.handleClose} />,
+      <FlatButton
+        label="Submit"
+        primary={true}
+        keyboardFocused={true}
+        onClick={() => {
+          this.setNewAppointment() 
+          this.handleClose();
+        }}
+      />
+    ];
+    return (
+      <div id="Calendar">
+        {/* react-big-calendar library utilized to render calendar*/}
+        <BigCalendar
+          events={this.state.events}
+          views={["month", "week", "day", "agenda"]}
+          timeslots={2}
+          defaultView="month"
+          defaultDate={new Date()}
+          selectable={true}
+          onSelectEvent={event => this.handleEventSelected(event)}
+          onSelectSlot={slotInfo => this.handleSlotSelected(slotInfo)}
+        />
+
+        {/* Material-ui Modal for booking new appointment */}
+        <Dialog
+          
+          title={`Book an appointment on ${moment(this.state.start).format(
+            "MMMM Do YYYY"
+          )}`}
+          actions={appointmentActions}
+          modal={false}
+          open={this.state.openSlot}
+          onRequestClose={this.handleClose}
+        >
+          
+          <div>
+
+     <ModalMenu 
+     updateCategory={this.categoryUpdate}
+        onChange={e => {
+          this.setCategory(e);
+        }}
+     />
+      
+    </div>
+    <h3>Category: {this.state.category}</h3>
+            <br />
+          <TextField
+            floatingLabelText="Title"
+            onChange={e => {
+              this.setTitle(e.target.value);
+            }}
+          />
+          <br />
+          <TextField
+            floatingLabelText="Description"
+            onChange={e => {
+              this.setDescription(e.target.value);
+            }}
+          />
+          <TimePicker
+            format="ampm"
+            floatingLabelText="Start Time"
+            minutesStep={1}
+            value={new Date(this.state.start)}
+            onChange={this.handleStartTime}
+          />
+          <TimePicker
+            format="ampm"
+            floatingLabelText="End Time"
+            minutesStep={1}
+            value={this.state.end}
+            onChange={this.handleEndTime}
+          />
+        </Dialog>
+
+        {/* Material-ui Modal for Existing Event */}
+        <Dialog
+          title={`View/Edit Appointment of ${moment(this.state.start).format(
+            "MMMM Do YYYY"
+          )}`}
+          actions={eventActions}
+          modal={false}
+          open={this.state.openEvent}
+          onRequestClose={this.handleClose}
+        >
+          <ModalMenu 
+          updateCategory={this.categoryUpdate}
+            defaultValue={this.state.category}
+            onChange={e => {
+              this.setCategory(e.target.value);
+            }}
+          /><br />
+          <h3>Category: {this.state.category}</h3>
+          <br />
+          <TextField
+            defaultValue={this.state.title}
+            floatingLabelText="Title"
+            onChange={e => {
+              this.setTitle(e.target.value);
+            }}
+          />
+          <br />
+          <TextField
+            floatingLabelText="Description"
+            multiLine={true}
+            defaultValue={this.state.desc}
+            onChange={e => {
+              this.setDescription(e.target.value);
+            }}
+          />
+          <TimePicker
+            format="ampm"
+            floatingLabelText="Start Time"
+            minutesStep={5}
+            value={this.state.start}
+            onChange={this.handleStartTime}
+          />
+          <TimePicker
+            format="ampm"
+            floatingLabelText="End Time"
+            minutesStep={5}
+            value={this.state.end}
+            onChange={this.handleEndTime}
+          />
+        </Dialog>
+      </div>
+    )
+  }
+}
+
+
+/*
+import React, { Component } from "react";
+import BigCalendar from "react-big-calendar";
+import moment from "moment";
+// import axios from 'axios';
+import ModalMenu from "./ModalMenu";
+import Dialog from "material-ui/Dialog";
+import FlatButton from "material-ui/FlatButton";
+import TextField from "material-ui/TextField";
+import TimePicker from "material-ui/TimePicker";
+import "../App.css";
+require("react-big-calendar/lib/css/react-big-calendar.css");
+
+
+BigCalendar.momentLocalizer(moment);
+
+export default class Calendar extends Component {
+  
+  constructor() {
+    super();
+    this.state = {
+      events: [],
+      category: "",
+      title: "",
+      start: {},
+      end: {},
+      desc: "",
+      openSlot: false,
+      openEvent: false,
+      eventList: "",
+      editForm: "",
       clickedEvent: {}
     };
     this.handleClose = this.handleClose.bind(this);
@@ -125,7 +522,7 @@ export default class Calendar extends Component {
     events.push(appointment);
    
         // and some other stuff
-		let route = `http://localhost:5556/api/events`;
+		let route = 'http://localhost:5556/api/events';
 		//let route = `https://helio-calendar-api.herokuapp.com/api/events`;
         let options = {
             method: 'POST',
@@ -263,7 +660,9 @@ export default class Calendar extends Component {
     ];
     return (
       <div id="Calendar">
-        {/* react-big-calendar library utilized to render calendar*/}
+        {/* react-big-calendar library utilized to render calendar*/
+        /*
+        }
         <BigCalendar
           events={this.state.events}
           views={["month", "week", "day", "agenda"]}
@@ -275,7 +674,9 @@ export default class Calendar extends Component {
           onSelectSlot={slotInfo => this.handleSlotSelected(slotInfo)}
         />
 
-        {/* Material-ui Modal for booking new appointment */}
+        {/* Material-ui Modal for booking new appointment */
+        /*
+        }
         <Dialog
           
           title={`Book an appointment on ${moment(this.state.start).format(
@@ -328,7 +729,10 @@ export default class Calendar extends Component {
           />
         </Dialog>
 
-        {/* Material-ui Modal for Existing Event */}
+        {/* Material-ui Modal for Existing Event */
+        
+          /*
+        }
         <Dialog
           title={`View/Edit Appointment of ${moment(this.state.start).format(
             "MMMM Do YYYY"
@@ -344,7 +748,10 @@ export default class Calendar extends Component {
               this.setCategory(e.target.value);
             }}
           /><br />
-          {/* <h3>Category: {this.state.category}</h3> */}
+          {/* <h3>Category: {this.state.category}</h3> */
+        
+          /*
+        }
           <br />
           <TextField
             defaultValue={this.state.title}
@@ -383,3 +790,4 @@ export default class Calendar extends Component {
 }
 
 
+*/
--- src/components/ModalMenu.js
+++ src/components/ModalMenu.js
@@ -14,6 +14,136 @@ import InboxIcon from '@material-ui/icons/MoveToInbox';
 import DraftsIcon from '@material-ui/icons/Drafts';
 import "../App.css";
 
+
+const StyledMenu = withStyles({
+  paper: {
+    border: '1px solid #d3d4d5',
+  },
+})((props) => (
+  <Menu
+    elevation={0}
+    getContentAnchorEl={null}
+    anchorOrigin={{
+      vertical: 'bottom',
+      horizontal: 'center',
+    }}
+    transformOrigin={{
+      vertical: 'top',
+      horizontal: 'center',
+    }}
+    {...props}
+  />
+));
+
+const StyledMenuItem = withStyles((theme) => ({
+  root: {
+    '&:focus': {
+      backgroundColor: theme.palette.primary.main,
+      '& .MuiListItemIcon-root, & .MuiListItemText-primary': {
+        color: theme.palette.common.white,
+      },
+    },
+  },
+}))(MenuItem);
+
+export default function ModalMenu(props) {
+  const [anchorEl, setAnchorEl] = React.useState(false);
+  console.log(props);
+
+  const handleClick = (event) => {
+    setAnchorEl(event.target);
+  };
+
+  const handleClose = (eventCategory) => {
+    props.updateCategory(eventCategory)
+    setAnchorEl(null);
+    
+  };
+
+  return (
+    <div>
+      <Button
+        aria-controls="customized-menu"
+        aria-haspopup="true"
+        variant="contained"
+        color="primary"
+        id="category"
+        onClick={handleClick}
+       
+      >
+        Choose Event Category
+      </Button>
+      <StyledMenu
+        style={{ zIndex: 1500 }}
+        id="customized-menu"
+        // anchorEl={anchorEl}
+        keepMounted
+        open={Boolean(anchorEl)}
+        onClose={handleClose}
+      >
+        <StyledMenuItem onClick={(e) => handleClose("Reminder")}>
+          <NotificationImportantIcon >
+            <NotificationImportantIcon fontSize="small" />
+          </NotificationImportantIcon>
+          <ListItemText primary="Reminder" />
+        </StyledMenuItem>
+
+        <StyledMenuItem onClick={(e) => handleClose('Appointment')}>
+          <PriorityHighIcon>
+            <DraftsIcon fontSize="small" />
+          </PriorityHighIcon>
+          <ListItemText primary="Appointment" />
+        </StyledMenuItem>
+
+        <StyledMenuItem onClick={(e) => handleClose('Meeting')}>
+          <GroupIcon>
+            <InboxIcon fontSize="small" />
+          </GroupIcon>
+          <ListItemText primary="Meeting" />
+        </StyledMenuItem>
+        <StyledMenuItem onClick={(e) => handleClose('Personal')}>
+          <SentimentSatisfiedAltIcon>
+            <InboxIcon fontSize="small" />
+          </SentimentSatisfiedAltIcon>
+          <ListItemText primary="Personal" />
+        </StyledMenuItem>
+        <StyledMenuItem onClick={(e) => handleClose('Work')}>
+          <WorkIcon>
+            <InboxIcon fontSize="small" />
+          </WorkIcon>
+          <ListItemText primary="Work" />
+        </StyledMenuItem>
+        <StyledMenuItem onClick={(e) => handleClose('Travel')}>
+          <FlightIcon>
+            <InboxIcon fontSize="small" />
+          </FlightIcon>
+          <ListItemText primary="Travel" />
+        </StyledMenuItem>
+      </StyledMenu>
+      
+    </div>
+  );
+}
+
+
+
+
+/* import React from 'react';
+import { withStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
+import FlightIcon from '@material-ui/icons/Flight';
+import WorkIcon from '@material-ui/icons/Work';
+import SentimentSatisfiedAltIcon from '@material-ui/icons/SentimentSatisfiedAlt';
+import PriorityHighIcon from '@material-ui/icons/PriorityHigh';
+import GroupIcon from '@material-ui/icons/Group';
+import NotificationImportantIcon from '@material-ui/icons/NotificationImportant';
+import ListItemText from '@material-ui/core/ListItemText';
+import InboxIcon from '@material-ui/icons/MoveToInbox';
+import DraftsIcon from '@material-ui/icons/Drafts';
+import "../App.css";
+
 const StyledMenu = withStyles({
   paper: {
     border: '1px solid #d3d4d5',
@@ -124,3 +254,5 @@ export default function ModalMenu(prop) {
     </div>
   );
 }
+
+*/
